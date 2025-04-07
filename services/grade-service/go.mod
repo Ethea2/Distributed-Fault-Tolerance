e315327c41b012d8622f4185286f947f89c0fd42
Ethea2 <nathandres2001@gmail.com>
2025-04-07 11:50:45 +0800

    Added: microservices

--- services/grade-service/go.mod
+++ services/grade-service/go.mod
@@ -1,3 +1,5 @@
 module github.com/Ethea2/Distributed-Fault-Tolerance/services/grade-service
 
-go 1.23.4
+go 1.19
+
+require github.com/go-chi/chi v1.5.5 // indirect
---
+++ services/grade-service/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Grade service is healthy"))
+	})
+
+	fmt.Println("Grade service starting on :8080")
+	http.ListenAndServe(":8080", r)
+}
